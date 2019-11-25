17d56a16338ca825b2fd4bed71704fcd95fee176
Rahul <rahulramesh26@yahoo.com>
2019-11-25 15:03:22 +0530

    added javascript

---
+++ pokemon.js
@@ -0,0 +1,5 @@
+var firstPokemon = prompt("enter your favourite first pokemon");
+var secondPokemon = prompt("enter your favourite seconf pokemon");
+var age= prompt(" What is your age?");
+console.log(" Your favourite first and second pokemon are =  \"" + firstPokemon +"\" and \"" + secondPokemon +"\".");
+console.log("your age is " + age);
